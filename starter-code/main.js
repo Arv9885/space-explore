0698dc7c5bd10d66db10dd74d3d12198964fa8d0
Arv9885 <arvindmangalarapu463@gmail.com>
2024-01-31 17:09:11 +0530

    break

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,18 +1,38 @@
-document.getElementById("crew-1").addEventListener("change", function () {
+document.getElementById("crew-1").addEventListener("change", () => {
+
   window.location.href = "crew-commander.html";
 });
 
-document.getElementById("crew-2").addEventListener("change", function () {
+document.getElementById("crew-2").addEventListener("change", () => {
   window.location.href = "crew-specialist.html";
 });
 
-document.getElementById("crew-3").addEventListener("change", function () {
+document.getElementById("crew-3").addEventListener("change", () => {
   window.location.href = "crew-pilot.html";
 });
 
-document.getElementById("crew-4").addEventListener("change", function () {
+document.getElementById("crew-4").addEventListener("change", () => {
   window.location.href = "crew-engineer.html";
 });
+ 
 
+//    function animateSpin() {
+//         var animatedLogo = document.getElementById("animated-logo");
 
-   
+//         // Add the "animate-spin" class for the spin animation
+//         animatedLogo.classList.add("animate");
+
+//         // Add the "sticky" class and dynamic top and left values
+//         animatedLogo.classList.add("fixed");
+//         animatedLogo.style.top = `${Math.round(Math.random() * 100)}px`;
+//         animatedLogo.style.left = `${Math.round(Math.random() * 100)}px`;
+
+//         // Log the updated classList for debugging
+//         console.log(animatedLogo.classList);
+//     }
+
+//     // Call the function to trigger the animation
+//     animateSpin();
+
+  
+
